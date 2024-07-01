133d2961372acb1220b265351f4675680058dd5d
Ashu23052002 <ans23052002@gmail.com>
2024-07-01 18:11:53 +0530

    authetication

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,8 @@ import Header from "./Header/Header.jsx";
 import Footer from "./Footer/Footer.jsx";
 import Container from "./container/Container.js";
 import LogoutBtn from "./Header/LogoutBtn.jsx";
+import Button from "./Button.jsx";
+import Input from "./Input.jsx";
+import Logo from "./Logo.jsx";
 
-export { Header, Footer, Container, LogoutBtn };
+export { Header, Footer, Container, LogoutBtn,Button,Input, Logo };
